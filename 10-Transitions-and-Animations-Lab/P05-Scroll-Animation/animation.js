3469db16f9b395be440f2924a327016cb6790fb2
Jeliozver <jelioradoev@gmail.com>
2018-05-18 14:30:24 +0300

    Added solutions for Transitions and Animations lab problems

---
+++ 10-Transitions-and-Animations-Lab/P05-Scroll-Animation/animation.js
@@ -0,0 +1,27 @@
+$(() => {
+    let $animation_elements = $('.animation-element');
+    let $window = $(window);
+
+    function check_if_in_view() {
+        let window_height = $window.height();
+        let window_top_position = $window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
+
+        $.each($animation_elements, function () {
+            let $element = $(this);
+            let element_height = $element.outerHeight();
+            let element_top_position = $element.offset().top;
+            let element_bottom_position = (element_top_position + element_height);
+
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+            } else {
+                $element.removeClass('in-view');
+            }
+        });
+    }
+
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+});
